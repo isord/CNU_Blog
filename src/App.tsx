dcfb4524db6a68e9db72102a2df1639d91983af7
Jominsung <isord@naver.com>
2024-05-30 13:45:05 +0900

    블로그 코드 작성 블로그 코드 재작성 react 과제 제출

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
-      <Router basename={'CNU_Blog'}>
+      <Router basename={'localhost:8000/CNU_Blog/'}>
         <Routes>
           <Route element={<Header />}>
             <Route element={<Layout />}>
--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'localhost:8000/CNU_Blog/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: '/CNU_Blog',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
